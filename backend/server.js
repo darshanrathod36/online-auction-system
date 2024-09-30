53adfd9e3e139271df35956ec6d9d42899148874
Darshan Rathod <darshanrathod3662@gmail.com>
2024-09-30 17:45:10 +0530

    backend commit

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,12 @@
-// Import required modules
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import app from "./app.js";
+import cloudinary from "cloudinary";
 
-// Import routes
-import authRoutes from './routes/authroutes.js';
-
-// Connect to the MongoDB database
-import connection from './db/connection.js';
-
-// Initialize the Express app
-const app = express();
-
-// Load environment variables from.env file
-dotenv.config();
-// Define the port number to run the server
-const PORT = process.env.PORT
-
-// Middleware
-app.use(express.json());
-app.use(cookieParser());
-
-// routes
-app.use('/api/auth', authRoutes);
-
-// Start the server and connect to the MongoDB database
-app.listen(PORT, () => {
-    connection(); // Connect to MongoDB
-    console.log(`server runing on port ${PORT}`)
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-
-
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`);
+});
